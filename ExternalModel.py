0524f739467796bbb45a54af28861691aeb84a15
Li Rongzhi <lirongzhi@Rongzhis-MacBook-Air.local>
2023-08-23 00:20:31 +0800

    remove unnecessary comments

--- ExternalModel.py
+++ ExternalModel.py
@@ -28,15 +28,6 @@ class ExternalModel:
         )
         return response.choices[0].message.content
 
-    # @staticmethod
-    # def calculate_token_num(prompt, template):
-    #     tokenizer = tiktoken.Tokenizer('gpt-3.5-turbo')
-    #     token_count = tokenizer.count_tokens(prompt)
-    #     token_num = token_count + 5
-    #     if template == PromptTemplate.GRAMMAR_CORRECTION:
-    #         token_num = 2 * token_count
-    #     return token_num
-
     @staticmethod
     def calculate_token_num(prompt, template):
         """Returns the number of tokens in a text string."""
@@ -46,24 +37,3 @@ class ExternalModel:
         if template == PromptTemplate.GRAMMAR_CORRECTION:
             result = 2 * num_tokens
         return result
-
-
-# key = "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"
-# model = ExternalModel(key)
-# content = "Category; Count; Price"
-# answer = model.get_response(content, PromptTemplate.ISHEADER_CHECK)
-# print(answer)
-# content = """Copyright © 20 20 NonDisclosureAgreement.com . All Rights Reserved.  Page 1 of 2 NON -DISCLOSURE AGREEMENT  (NDA)
-# This Nondisclosure Agreement or ("Agreement") has been  entered into on the date of
-# ______________________________ and is by and between :"""
-# answer = model.get_response(content, PromptTemplate.GRAMMAR_CORRECTION)
-# print(answer)
-# footnotes = {'0': ['Copyright © 2020 NonDisclosureAgreement.com. All Rights Reserved.', 'Page 1 of 2'],
-#              '1': ['Copyright © 2020 NonDisclosureAgreement.com. All Rights Reserved.', 'Page 2 of 2']}
-#
-# for footnote in footnotes['0']:
-#     if footnote in answer:
-#         answer = answer.replace(footnote, "")
-#         print(footnote)
-#
-# print(answer)
